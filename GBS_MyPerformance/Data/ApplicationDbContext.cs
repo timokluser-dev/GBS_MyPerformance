38204a3784e8300b59bf09fd1110d374af67ab13
timokluser-dev <timo@timokluser.ch>
2021-12-22 20:08:40 +0100

    feat: prepare DbSets

--- GBS_MyPerformance/Data/ApplicationDbContext.cs
+++ GBS_MyPerformance/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using GBS_MyPerformance.Identity.Models;
+using GBS_MyPerformance.Models;
 using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,24 @@ namespace GBS_MyPerformance.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        #region DbSets GBS_MyPerformance
+
+        //public DbSet<Profession> Professions { get; set; }
+        //public DbSet<ProfessionArea> ProfessionAreas { get; set; }
+        //public DbSet<Semester> Semesters { get; set; }
+        //public DbSet<SchoolClass> SchoolClasses { get; set; }
+        //public DbSet<Teacher> Teachers { get; set; }
+        //public DbSet<Student> Students { get; set; }
+        //public DbSet<Company> Companies { get; set; }
+        //public DbSet<ApprenticeTrainer> ApprenticeTrainers { get; set; }
+        //public DbSet<Subject> Subjects { get; set; }
+        //public DbSet<Rating> Ratings { get; set; }
+        //public DbSet<SemesterRating> SemesterRatings { get; set; }
+        //public DbSet<Mark> Marks { get; set; }
+        //public DbSet<RatingCategory> RatingCategories { get; set; }
+
+        #endregion DbSets GBS_MyPerformance
+
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
