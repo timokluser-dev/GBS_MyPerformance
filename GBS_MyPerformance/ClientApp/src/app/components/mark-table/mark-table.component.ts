545422e3381b8cfc81699625cba08214868e61ee
timokluser-dev <timo@timokluser.ch>
2021-12-23 15:48:36 +0100

    chore: migrate to scss

--- GBS_MyPerformance/ClientApp/src/app/components/mark-table/mark-table.component.ts
+++ GBS_MyPerformance/ClientApp/src/app/components/mark-table/mark-table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mark-table',
   templateUrl: './mark-table.component.html',
-  styleUrls: ['./mark-table.component.css']
+  styleUrls: ['./mark-table.component.scss']
 })
 export class MarkTableComponent implements OnInit {
 
--- GBS_MyPerformance/ClientApp/src/app/components/students-mark-table/students-mark-table.component.ts
+++ GBS_MyPerformance/ClientApp/src/app/components/students-mark-table/students-mark-table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-students-mark-table',
   templateUrl: './students-mark-table.component.html',
-  styleUrls: ['./students-mark-table.component.css']
+  styleUrls: ['./students-mark-table.component.scss']
 })
 export class StudentsMarkTableComponent implements OnInit {
 
