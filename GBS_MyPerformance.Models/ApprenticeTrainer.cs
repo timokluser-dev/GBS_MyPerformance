55601134fb56dea8c70a8b94bcb786f8aa5ed944
timokluser-dev <timo@timokluser.ch>
2021-12-28 15:38:09 +0100

    docs: update models

--- GBS_MyPerformance.Models/ApprenticeTrainer.cs
+++ GBS_MyPerformance.Models/ApprenticeTrainer.cs
@@ -6,6 +6,9 @@ namespace GBS_MyPerformance.Models
 {
     public class ApprenticeTrainer : ApplicationUser
     {
+        /// <summary>
+        /// all apprentices, supervised by the trainer
+        /// </summary>
         public List<Student> Apprentices { get; set; }
 
         public ApprenticeTrainer()
