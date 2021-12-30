c99b658289e397dc42f23fb19f72ec7fed3fb58c
timokluser-dev <timo@timokluser.ch>
2021-12-30 22:59:22 +0100

    style: reformat code

--- GBS_MyPerformance/ClientApp/src/app/pages/portals/teacher/classes-overview-page-component/classes-overview-page-component.component.ts
+++ GBS_MyPerformance/ClientApp/src/app/pages/portals/teacher/classes-overview-page-component/classes-overview-page-component.component.ts
@@ -2,14 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {data} from '../mock';
 
 @Component({
-  selector: 'app-classes-overview-page-component',
-  templateUrl: './classes-overview-page-component.component.html',
-  styleUrls: ['./classes-overview-page-component.component.scss'],
+    selector: 'app-classes-overview-page-component',
+    templateUrl: './classes-overview-page-component.component.html',
+    styleUrls: ['./classes-overview-page-component.component.scss'],
 })
 export class ClassesOverviewPageComponentComponent implements OnInit {
-  public breadcrumbs = [{name: 'Klassen', link: null}];
-  public classes = data;
-  constructor() {}
+    public breadcrumbs = [{name: 'Klassen', link: null}];
+    public classes = data;
 
-  ngOnInit() {}
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 }
