f7c7db5a1b3b45491ddc3f5709543ae85e83404e
timokluser-dev <timo@timokluser.ch>
2022-06-13 22:15:24 +0200

    feat: add example how to use ApiClients

--- GBS_MyPerformance/ClientApp/src/app/pages/portals/teacher/classes-overview-page-component/classes-overview-page-component.component.ts
+++ GBS_MyPerformance/ClientApp/src/app/pages/portals/teacher/classes-overview-page-component/classes-overview-page-component.component.ts
@@ -1,18 +1,25 @@
 import {Component, OnInit} from '@angular/core';
+import {SchoolClassService, WeatherForecastService} from 'myperformance-client';
 import {data} from '../mock';
 
 @Component({
-    selector: 'app-classes-overview-page-component',
-    templateUrl: './classes-overview-page-component.component.html',
-    styleUrls: ['./classes-overview-page-component.component.scss'],
+  selector: 'app-classes-overview-page-component',
+  templateUrl: './classes-overview-page-component.component.html',
+  styleUrls: ['./classes-overview-page-component.component.scss'],
 })
 export class ClassesOverviewPageComponentComponent implements OnInit {
-    public breadcrumbs = [{name: 'Klassen', link: null}];
-    public classes = data;
+  public breadcrumbs = [{name: 'Klassen', link: null}];
+  public classes = data;
 
-    constructor() {
-    }
+  constructor(private api: SchoolClassService, private api2: WeatherForecastService) {}
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+    this.api.apiSchoolClassGet().subscribe(data => {
+      console.log(data.map(schoolClass => schoolClass.id));
+    });
+
+    this.api2.apiWeatherForecastGet().subscribe(data => {
+      console.log(data.map(weatherForecast => weatherForecast.summary));
+    });
+  }
 }
