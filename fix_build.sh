e0a908a6c54d972cf60b3863e5d7dd06619fb02c
timokluser-dev <timo@timokluser.ch>
2022-06-14 18:48:02 +0200

    feat: add fix_build.sh

---
+++ fix_build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+# load nvm
+. ~/.nvm/nvm.sh
+
+BASE_PATH=$(pwd)
+
+npm run docker:down
+
+npm run docker:up
+
+nvm use
+
+# npm i
+
+chmod +x ./GBS_MyPerformance/ClientApp/projects/build.sh
+
+cd ./GBS_MyPerformance/ClientApp/projects
+./build.sh
+
+cd $BASE_PATH
+
+npm run backend:start
