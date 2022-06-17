9de8c1f56ae53b5290c84d62079b1eeb32acf921
timokluser-dev <timo@timokluser.ch>
2022-06-17 17:28:49 +0200

    add edit time span insert migration

---
+++ GBS_MyPerformance/Data/Migrations/20220617151310_add_edit_time_span.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GBS_MyPerformance.Data.Migrations
+{
+    public partial class add_edit_time_span : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO dbo.EditTimeSpans([Id],[From],[To]) VALUES ('00000000-0000-0000-0000-000000000001','2022-01-01 00:00:00','2022-12-31 23:59:59');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM dbo.EditTimeSpans WHERE [Id] = '00000000-0000-0000-0000-000000000001';)");
+        }
+    }
+}
